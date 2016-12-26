eb1267c1befb3162d6f61048bd03c7cfb38c5ebd
shiqkuangsan <761105568@qq.com>
2016-12-26 17:04:55 +0800

    fix

--- app/src/main/java/com/shiqkuangsan/mycustomviews/ui/activity/MainActivity.java
+++ app/src/main/java/com/shiqkuangsan/mycustomviews/ui/activity/MainActivity.java
@@ -68,6 +68,19 @@ public class MainActivity extends BaseActivity {
     public void processClick(View v) {
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mLocationClient != null)
+            mLocationClient.stopLocation();//停止定位后，本地定位服务并不会被销毁
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mLocationClient != null)
+            mLocationClient.onDestroy();//销毁定位客户端，同时销毁本地定位服务。
+    }
 
     /**
      * 轮播图
