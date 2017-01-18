854b62ad6744c6a5dc9f789476dd0ab6c20bb54f
shiqkuangsan <761105568@qq.com>
2017-01-18 20:54:54 +0800

    添加新module.

---
+++ rxandroid&mvp/src/main/java/com/shiqkuangsan/rxandroidmvp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.shiqkuangsan.rxandroidmvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rxandroidmvp.R;
+
+/**
+ * Created by shiqkuangsan on 2017/1/18.
+ * <p>
+ * ClassName: MainActivity
+ * Author: shiqkuangsan
+ * Description: 首页面
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
