449928f3f11609f822845b8fdb9eb68aa9848bde
shiqkuangsan <761105568@qq.com>
2017-01-19 10:35:32 +0800

    添加rx包的的相关依赖

--- rxandroidmvp/src/main/java/com/shiqkuangsan/rxandroidmvp/MainActivity.java
+++ rxandroidmvp/src/main/java/com/shiqkuangsan/rxandroidmvp/MainActivity.java
@@ -2,6 +2,12 @@ package com.shiqkuangsan.rxandroidmvp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlSerializer;
+
+import static android.view.View.X;
 
 /**
  * Created by shiqkuangsan on 2017/1/18.
