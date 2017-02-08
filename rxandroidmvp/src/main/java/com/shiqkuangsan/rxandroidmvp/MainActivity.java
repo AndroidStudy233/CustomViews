5f595613d088dc7d028bd301e8f72283489fba36
yuzhiwei <yuzhiwei>
2017-02-08 15:30:11 +0800

    完善rxjava基本介绍，添加rxjava线程变换例子

--- rxandroidmvp/src/main/java/com/shiqkuangsan/rxandroidmvp/MainActivity.java
+++ rxandroidmvp/src/main/java/com/shiqkuangsan/rxandroidmvp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.shiqkuangsan.rxandroidmvp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
 
 /**
  * Created by shiqkuangsan on 2017/1/18.
@@ -17,13 +22,25 @@ import rx.Subscriber;
  */
 public class MainActivity extends AppCompatActivity {
     private TextView tv;
+    private  Observable observable ;
+    private  Observer observer;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv= (TextView) findViewById(R.id.tv);
-        baseImp();
+        button = (Button) findViewById(R.id.btn);
+        baseImp();//  这些全都是在同一个线程的观察者 ，本篇概念看完  能了解基本用法，下一遍异步观察~ RxSchedulerActivity
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RxSchedulerActivity.class));
+            }
+        });
     }
+    
+    
     /*** rx 概念:
      *   Observable (可观察者，即被观察者)、
      *   Observer (观察者)、 subscribe (订阅)、事件。
@@ -40,16 +57,32 @@ public class MainActivity extends AppCompatActivity {
     public void baseImp(){
         /**
          * 1)创建 Observer   它决定事件触发的时候将有怎样的行为  (观察者)   
-         * 1.1)除了 Observer 接口之外，RxJava 还内置了一个实现了 Observer 的抽象类：
-         * Subscriber。 Subscriber 对 Observer 接口进行了一些扩展
-         * 但他们的基本使用方式是完全一样的
-         *
          * 2)创建 Observable（被观察者）
          * 3)Subscribe (订阅)
          *
          * */
         //1
-        Observer<String> observer = new Observer<String>() {
+        Observer observer = createObserver();
+        //2  
+        Observable observable = createObservable();
+        //3
+        subscriberImp();
+       
+    }
+
+    /**
+     * 1)创建 Observer   它决定事件触发的时候将有怎样的行为  (观察者)   
+     * 1.1)除了 Observer 接口之外，RxJava 还内置了一个实现了 Observer 的抽象类：
+     * Subscriber。 Subscriber 对 Observer 接口进行了一些扩展
+     * 但他们的基本使用方式是完全一样的
+     *
+     * 2)创建 Observable（被观察者）
+     * 3)Subscribe (订阅)
+     *
+     * */
+    public Observer createObserver(){
+        //1
+        observer = new Observer<String>() {
             @Override
             public void onCompleted() {
                 MyLogUtil.i("完成时调用");
@@ -67,29 +100,25 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         //1.1 
-        Subscriber<String> subscriber = new Subscriber<String>() {
-            @Override
-            public void onCompleted() {
-                MyLogUtil.i("完成时调用");
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                MyLogUtil.i("异常时调用");
-            }
-
-            @Override
-            public void onNext(String s) {
-                MyLogUtil.i("被观察者发生改变，观察者接下来的动作");
-            }
-        };
-        //2  
-        Observable observable = createObservable();
-        //3
-        observable.subscribe(observer);
-        //or    observable.subscribe(subscriber);
-       
+//        observer = new Subscriber<String>() {
+//            @Override
+//            public void onCompleted() {
+//                MyLogUtil.i("完成时调用");
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                MyLogUtil.i("异常时调用");
+//            }
+//
+//            @Override
+//            public void onNext(String s) {
+//                MyLogUtil.i("被观察者发生改变，观察者接下来的动作");
+//            }
+//        };
+        return observer;
     }
+    
     /**  
      *  observable(被观察者)
      *  create() 方法是 RxJava 最基本的创造事件序列的方法。基于这个方法， RxJava 还提供了一些方法用来快捷创建事件队列，例如：
@@ -98,14 +127,14 @@ public class MainActivity extends AppCompatActivity {
      *  just(T...) 的例子和 from(T[]) 的例子，都和之前的 create(OnSubscribe) 的例子是等价的。
      **/
     public Observable createObservable(){
-        Observable ob1 =null;
 //        ob1 = Observable.just("1","2","3");
+        
 //        String [] ss = {"1","2","3"};
 //        ob1 = Observable.from(ss);
         
         //上面的两种方式和Observable.create等价
-       
-        ob1 =Observable.create(new Observable.OnSubscribe<String>() {
+
+        observable =Observable.create(new Observable.OnSubscribe<String>() {
             @Override
             public void call(Subscriber<? super String> subscriber) {
                 try{
@@ -120,6 +149,46 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        return ob1;
+        return observable;
+    }
+    
+    /**
+     * 除了 subscribe(Observer) 和 subscribe(Subscriber) ，
+     * subscribe() 还支持不完整定义的回调，RxJava 会自动根据定义创建出 Subscriber 。形式如下：
+     * */
+    public void subscriberImp(){
+        /**
+         * 简单解释一下这段代码中出现的 Action1 和 Action0。 Action0 是 RxJava 的一个接口，它只有一个方法 call()，
+         * 这个方法是无参无返回值的；由于 onCompleted() 方法也是无参无返回值的，因此 Action0 可以被当成一个包装对象，
+         * 将 onCompleted() 的内容打包起来将自己作为一个参数传入 subscribe() 以实现不完整定义的回调。这样其实也可以
+         * 看做将 onCompleted() 方法作为参数传进了 subscribe()，相当于其他某些语言中的『闭包』。 Action1 也是一个接口，
+         * 它同样只有一个方法 call(T param)，这个方法也无返回值，但有一个参数；与 Action0 同理，由于 onNext(T obj) 和
+         * onError(Throwable error) 也是单参数无返回值的，因此 Action1 可以将 onNext(obj) 和 onError(error) 打包起来传入 
+         * subscribe() 以实现不完整定义的回调。事实上，虽然 Action0 和 Action1 在 API 中使用最广泛，
+         * 但 RxJava 是提供了多个 ActionX 形式的接口 (例如 Action2, Action3) 的，它们可以被用以包装不同的无返回值的方法
+         * */
+        Action1<String> action1 = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                MyLogUtil.e("onNext"+s);
+            }
+        };
+        Action1<Throwable> error = new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                MyLogUtil.e("onError");
+            }
+        };
+        Action0 complete = new Action0() {
+            @Override
+            public void call() {
+                MyLogUtil.e("onComplete");
+            }
+        };
+        observable.subscribe(action1);    //onNext
+        observable.subscribe(action1,error) ;//onNext  onError
+        observable.subscribe(action1,error,complete);//onNext  onError  complete
+        
+        //或者直接 observable.subscribe(observer);
     }
 }
---
+++ rxandroidmvp/src/main/java/com/shiqkuangsan/rxandroidmvp/RxSchedulerActivity.java
@@ -0,0 +1,102 @@
+package com.shiqkuangsan.rxandroidmvp;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
+/*************************************************
+ * <p>类描述 rx的线程变换</p>
+ * <p>创建人：余志伟</p>
+ * <p>创建时间 : 2017/2/8</p>
+ * <p>修改人：       </p>
+ * <p>修改时间：   </p>
+ * <p>修改备注：   </p>
+ *
+ * @version V3.1
+ *********************************/
+public class RxSchedulerActivity extends AppCompatActivity {
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rxscheduler);
+        imageView = (ImageView) findViewById(R.id.img);
+        changeScheduler();
+    }
+    /**  
+     * 在不指定线程的情况下， RxJava 遵循的是线程不变的原则，即：在哪个线程调用 subscribe()，就在哪个线程生产事件；
+     * 在哪个线程生产事件，就在哪个线程消费事件。如果需要切换线程，就需要用到 Scheduler （调度器）。
+     * 
+     * Schedulers.immediate(): 直接在当前线程运行，相当于不指定线程。这是默认的 Scheduler。
+     * 
+     * Schedulers.newThread(): 总是启用新线程，并在新线程执行操作。
+     * 
+     * Schedulers.io(): I/O 操作（读写文件、读写数据库、网络信息交互等）所使用的 Scheduler。
+     * 行为模式和 newThread() 差不多，区别在于 io() 的内部实现是是用一个无数量上限的线程池，
+     * 可以重用空闲的线程，因此多数情况下 io() 比 newThread() 更有效率。不要把计算工作放在 io() 中，可以避免创建不必要的线程。
+     * 
+     * Schedulers.computation(): 计算所使用的 Scheduler。这个计算指的是 CPU 密集型计算，即不会被 I/O 等操作限制性能的操作，
+     * 例如图形的计算。这个 Scheduler 使用的固定的线程池，大小为 CPU 核数。不要把 I/O 操作放在 computation() 中，否则 I/O 操作的等待时间会浪费 CPU。
+     * 
+     * 另外， Android 还有一个专用的 AndroidSchedulers.mainThread()，它指定的操作将在 Android 主线程运行。
+     * 有了这几个 Scheduler ，就可以使用 subscribeOn() 和 observeOn() 两个方法来对线程进行控制了。 
+     * 
+     *   subscribeOn(): 指定 subscribe() 所发生的线程，即 Observable.OnSubscribe 被激活时所处的线程。或者叫做事件产生的线程。 (我的理解是被观察者事件创建))
+     * 
+     *   observeOn(): 指定 Subscriber 所运行在的线程。或者叫做事件消费的线程。(我的理解是观察者的事件触发)
+     * 
+     * */
+    public void changeScheduler(){
+        Observable.just(1, 2, 3, 4)
+                .subscribeOn(Schedulers.io()) // 指定 subscribe() 发生在 IO 线程        
+                .observeOn(AndroidSchedulers.mainThread()) // 指定 Subscriber 的回调发生在主线程()   
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer number) {
+                        Log.d("aaa", "number:" + number);
+                    }
+                });
+
+        Observable.create(new Observable.OnSubscribe<Drawable>() {
+            @Override
+            public void call(Subscriber<? super Drawable> subscriber) {
+                try {
+                    Thread.sleep(10 * 1000);//这里线程由subscribeOn决定
+                    Drawable drawable = getResources().getDrawable(R.mipmap.ic_launcher);
+                    subscriber.onNext(drawable);
+                    subscriber.onCompleted();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    subscriber.onError(e);
+                }
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Drawable>() {
+                    //以下线程由observeOn决定
+                    @Override
+                    public void onCompleted() {
+                        Log.e("GG", "onCompleted");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("GG", e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(Drawable drawable) {
+                        imageView.setImageDrawable(drawable);
+                    }
+                });
+    }
+}
