03d90044e5b340057dabce1e2caab64bf71afebb
shiqkuangsan <761105568@qq.com>
2017-04-27 13:04:17 +0800

    调整MyLogUtil, 添加重载自定义tag 日志输出方法

--- app/src/main/java/com/shiqkuangsan/mycustomviews/utils/MyLogUtil.java
+++ app/src/main/java/com/shiqkuangsan/mycustomviews/utils/MyLogUtil.java
@@ -10,82 +10,162 @@ import android.util.Log;
  * Description: 日志输出的util
  */
 public class MyLogUtil {
-	/** 日志输出级别NONE */
+
+	/**
+	 * 日志输出级别NONE
+	 */
 	private static final int LEVEL_NONE = 0;
-	/** 日志输出级别E */
-	private static final int LEVEL_ERROR =1;
-	/** 日志输出级别W */
+	/**
+	 * 日志输出级别E
+	 */
+	private static final int LEVEL_ERROR = 1;
+	/**
+	 * 日志输出级别W
+	 */
 	private static final int LEVEL_WARN = 2;
-	/** 日志输出级别I */
+	/**
+	 * 日志输出级别I
+	 */
 	private static final int LEVEL_INFO = 3;
-	/** 日志输出级别D */
+	/**
+	 * 日志输出级别D
+	 */
 	private static final int LEVEL_DEBUG = 4;
-	/** 日志输出级别V */
+	/**
+	 * 日志输出级别V
+	 */
 	private static final int LEVEL_VERBOSE = 5;
 
-	/** 日志输出时的TAG */
-	private static String mTag = "CLogUtil";
+	/**
+	 * 日志输出时的TAG
+	 */
+	private static String mTag = "MyLogUtil";
 
-	/** 是否允许输出log,该值控制着是否允许打印log*/
+	/**
+	 * 是否允许输出log,该值控制着是否允许打印log
+	 */
 	private static int mDebuggable = LEVEL_VERBOSE;
 
-	/** 以级别为 v 的形式输出LOG */
+	/**
+	 * 以级别为 v 的形式输出LOG
+	 */
 	public static void v(String msg) {
 		if (mDebuggable >= LEVEL_VERBOSE) {
 			Log.v(mTag, msg);
 		}
 	}
 
-	/** 以级别为 d 的形式输出LOG */
+	/**
+	 * 以级别为 v 的形式输出LOG, 自定义TAG
+	 */
+	public static void v(String tag, String msg) {
+		if (mDebuggable >= LEVEL_VERBOSE) {
+			Log.v(tag, msg);
+		}
+	}
+
+	/**
+	 * 以级别为 d 的形式输出LOG
+	 */
 	public static void d(String msg) {
 		if (mDebuggable >= LEVEL_DEBUG) {
 			Log.d(mTag, msg);
 		}
 	}
 
-	/** 以级别为 i 的形式输出LOG */
+	/**
+	 * 以级别为 d 的形式输出LOG, 自定义TAG
+	 */
+	public static void d(String tag, String msg) {
+		if (mDebuggable >= LEVEL_DEBUG) {
+			Log.d(tag, msg);
+		}
+	}
+
+	/**
+	 * 以级别为 i 的形式输出LOG
+	 */
 	public static void i(String msg) {
 		if (mDebuggable >= LEVEL_INFO) {
 			Log.i(mTag, msg);
 		}
 	}
 
-	/** 以级别为 w 的形式输出LOG */
+	/**
+	 * 以级别为 i 的形式输出LOG, 自定义TAG
+	 */
+	public static void i(String tag, String msg) {
+		if (mDebuggable >= LEVEL_INFO) {
+			Log.i(tag, msg);
+		}
+	}
+
+	/**
+	 * 以级别为 w 的形式输出LOG
+	 */
 	public static void w(String msg) {
 		if (mDebuggable >= LEVEL_WARN) {
 			Log.w(mTag, msg);
 		}
 	}
 
-	/** 以级别为 w 的形式输出Throwable */
+	/**
+	 * 以级别为 w 的形式输出LOG, 自定义TAG
+	 */
+	public static void w(String tag, String msg) {
+		if (mDebuggable >= LEVEL_WARN) {
+			Log.w(tag, msg);
+		}
+	}
+
+	/**
+	 * 以级别为 w 的形式输出Throwable
+	 */
 	public static void w(Throwable tr) {
 		if (mDebuggable >= LEVEL_WARN) {
 			Log.w(mTag, "", tr);
 		}
 	}
 
-	/** 以级别为 w 的形式输出LOG信息和Throwable */
+	/**
+	 * 以级别为 w 的形式输出LOG信息和Throwable
+	 */
 	public static void w(String msg, Throwable tr) {
 		if (mDebuggable >= LEVEL_WARN && null != msg) {
 			Log.w(mTag, msg, tr);
 		}
 	}
 
-	/** 以级别为 e 的形式输出LOG */
+	/**
+	 * 以级别为 e 的形式输出LOG
+	 */
 	public static void e(String msg) {
 		if (mDebuggable >= LEVEL_ERROR) {
 			Log.e(mTag, msg);
 		}
 	}
 
-	/** 以级别为 e 的形式输出Throwable */
+	/**
+	 * 以级别为 e 的形式输出LOG, 自定义TAG
+	 */
+	public static void e(String tag, String msg) {
+		if (mDebuggable >= LEVEL_ERROR) {
+			Log.e(tag, msg);
+		}
+	}
+
+	/**
+	 * 以级别为 e 的形式输出Throwable
+	 */
 	public static void e(Throwable tr) {
 		if (mDebuggable >= LEVEL_ERROR) {
 			Log.e(mTag, "", tr);
 		}
 	}
 
-	/** 以级别为 e 的形式输出LOG信息和Throwable */
+	/**
+	 * 以级别为 e 的形式输出LOG信息和Throwable
+	 */
 	public static void e(String msg, Throwable tr) {
 		if (mDebuggable >= LEVEL_ERROR && null != msg) {
 			Log.e(mTag, msg, tr);
