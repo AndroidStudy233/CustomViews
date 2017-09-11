caa985e69f2c26d569f77668da941fe30f9d7241
shiqkuangsan <761105568@qq.com>
2017-09-11 16:31:33 +0800

    add note

--- app/src/main/java/com/shiqkuangsan/mycustomviews/ui/activity/FragmentAdvancedActivity.java
+++ app/src/main/java/com/shiqkuangsan/mycustomviews/ui/activity/FragmentAdvancedActivity.java
@@ -1,5 +1,7 @@
 package com.shiqkuangsan.mycustomviews.ui.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -9,4 +11,9 @@ import android.support.v7.app.AppCompatActivity;
  * Description: 关于fragment的技术点.包括切换、添加子frag、回退处理、沉浸式等等
  */
 public class FragmentAdvancedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
