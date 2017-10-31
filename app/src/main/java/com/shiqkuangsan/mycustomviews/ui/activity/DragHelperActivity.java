2ca77c4eb524170869e6438edd99b82c47aa71b9
MrUncleYuzw <754519420@qq.com>
2017-10-31 17:59:33 +0800

    增加draghelper示例

---
+++ app/src/main/java/com/shiqkuangsan/mycustomviews/ui/activity/DragHelperActivity.java
@@ -0,0 +1,20 @@
+package com.shiqkuangsan.mycustomviews.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.shiqkuangsan.mycustomviews.R;
+
+
+/**
+ * Created by yu on 2017/10/31.
+ */
+
+public class DragHelperActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draghelper);
+    }
+}
